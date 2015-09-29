b582b21826e782417231dd0bab3c622c5be2d813
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2015-09-29 19:31:02 +0800

    Updated MainActivity.java

--- app/src/main/java/com/cabatuan/weekday/MainActivity.java
+++ app/src/main/java/com/cabatuan/weekday/MainActivity.java
@@ -1,10 +1,15 @@
-package com.cabatuan.weekday;
+package io.github.melvincabatuan.secondactivitydemo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,8 +17,12 @@ import java.util.IllegalFormatException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_out;
-    private int day, month, year, day_out;
+    private Integer day;
+    private Integer month;
+    private Integer year;
+    private Integer century;
+    private Integer day_out;
+    private String s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,36 +31,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getInputValues(){
-        EditText et_day = (EditText) findViewById(R.id.edittextmonth);
+
+        EditText et_day = (EditText) findViewById(R.id.edittextday);
         EditText et_month = (EditText) findViewById(R.id.edittextmonth);
-        EditText et_year = (EditText) findViewById(R.id.edittextmonth);
+        EditText et_year = (EditText) findViewById(R.id.edittextyear);
 
         try {
-             day = Integer.parseInt(et_day.getText().toString());
+            day = Integer.parseInt(et_day.getText().toString());
         }
-        catch (IllegalFormatException e){
-             day = 0;
+        catch (Exception e){
+            day = 0;
         }
 
         try {
             month = Integer.parseInt(et_month.getText().toString());
         }
-        catch (IllegalFormatException e){
+        catch (Exception e){
             month = 0;
         }
 
+        if (month == 1)
+            month = 13;
+        else if (month == 2)
+            month = 14;
+
         try {
-             year = Integer.parseInt(et_year.getText().toString());
+            s = et_year.getText().toString();
+            year = Integer.parseInt(s.substring(2));
+            century =  Integer.parseInt(s.substring(0,2));
         }
-        catch (IllegalFormatException e){
+        catch (Exception e){
             year = 0;
+            century = 0;
         }
 
+        // Update the TextViews (in case an entry was left out)
+        et_day.setText(day.toString());
+        et_month.setText(month.toString());
+        et_year.setText(s);
     }
 
     private void computeDay(){
         // TODO: Give the formula here:
-        day_out = 0;
+
+        Log.d("computeDay()","s = "  + s + ", year = " + Integer.toString(year) + ", " + "century = " + Integer.toString(century));
+
+        day_out = (day + (int)(26 *(month + 1)/10.0) + year + (int)(year/4.0) + (int)(century/4.0) + 5 * century ) % 7;
     }
 
     public void onClickCompute(View view){
@@ -60,60 +85,44 @@ public class MainActivity extends AppCompatActivity {
         printDay();
     }
 
-private void printDay(){
-    String day_text;
-    switch(day_out) {
-        case 0:
-            day_text = "It's Saturday!";
-            break;
-        case 1:
-            day_text = "It's Sunday!";
-            break;
-        case 2:
-            day_text = "It's Monday!";
-            break;
-        case 3:
-            day_text = "It's Tuesday!";
-            break;
-        case 4:
-            day_text ="It's Wednesday!";
-            break;
-        case 5:
-            day_text ="It's Thursday!";
-            break;
-        case 6:
-            day_text ="It's Friday!";
-            break;
-        default:
-            day_text ="Invalid day!";
-    }
-
-    // Set the textview text
-    tv_out = (TextView) findViewById(R.id.textviewoutput);
-    tv_out.setText(day_text);
-}
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void printDay(){
+        String day_text;
+        switch(day_out) {
+            case 0:
+                day_text = "It's Saturday!";
+                break;
+            case 1:
+                day_text = "It's Sunday!";
+                break;
+            case 2:
+                day_text = "It's Monday!";
+                break;
+            case 3:
+                day_text = "It's Tuesday!";
+                break;
+            case 4:
+                day_text ="It's Wednesday!";
+                break;
+            case 5:
+                day_text ="It's Thursday!";
+                break;
+            case 6:
+                day_text ="It's Friday!";
+                break;
+            default:
+                day_text ="Invalid day!";
         }
 
-        return super.onOptionsItemSelected(item);
+        // Intent to go to the second activity
+
+        Intent i = new Intent(this, SecondActivity.class);
+
+        // Put attachment data along with the intent
+        i.putExtra("message", day_text);
+
+        startActivity(i);
     }
+
+
+
 }
